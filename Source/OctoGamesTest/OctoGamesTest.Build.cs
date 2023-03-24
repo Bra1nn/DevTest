b5350e703b482c2e42b59c2a9418120d594679b4
Artem Mayangin <a.mayangin@dragon-machines.com>
2023-03-24 15:28:33 +0400

    Added source modules for cinematics

--- Source/OctoGamesTest/OctoGamesTest.Build.cs
+++ Source/OctoGamesTest/OctoGamesTest.Build.cs
@@ -8,6 +8,6 @@ public class OctoGamesTest : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "LevelSequence", "MovieScene" });
 	}
 }
